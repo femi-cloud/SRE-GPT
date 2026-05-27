0fb658732698f719df845cef4d6ae3b9aa401b0c
femi-cloud <femi13sanya@gmail.com>
2026-05-27 23:12:46 +0100

    fix: dashboards connectivy....

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import {defineConfig} from 'vite';
 export default defineConfig(() => {
   return {
     plugins: [react(), tailwindcss()],
+    publicDir: 'dashboard',
     resolve: {
       alias: {
         '@': path.resolve(__dirname, '.'),
